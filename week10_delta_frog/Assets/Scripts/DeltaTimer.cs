5c9377beb4af1f99f0e2ff146421afa8f458f05c
Charlie Harper <charlottehdesigns@gmail.com>
2019-11-14 15:36:37 -0500

    Added in a simple countdown script -Uses in scene objects -Provides game state text changes -Counts down from 10 at the moment

---
+++ week10_delta_frog/Assets/Scripts/DeltaTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeltaTimer : MonoBehaviour
+{
+    public float TimerAmount = 10; //how long the countdown timer is
+    public string GoalMessage; //instructions on how to play the game
+    public string SuccessMessage; //WinGame message
+    public string FailureMessage; //Game Over Message
+
+    public Text GameText; //The object with the "messages" text component
+    public Text TimerText; //The object for the timer text
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameText.text = GoalMessage; //Load in instructions
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerAmount -= Time.deltaTime; //countdown from timer amount
+
+        if(TimerAmount<=0) // when it hits zero
+        {
+            GameText.text = FailureMessage; //you have died
+        }
+        
+
+    }
+}
