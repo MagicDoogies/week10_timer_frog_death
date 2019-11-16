3ae3f8d716018b1c8e2a3fc0f9371d6557ccd3ca
zeeodd <evergreenskier@gmail.com>
2019-11-16 10:07:12 -0500

    1. Added a camera animation 2. Tweaked some numbers so the frog has a satisfying death

--- week10_delta_frog/Assets/Scripts/DeltaTimer.cs
+++ week10_delta_frog/Assets/Scripts/DeltaTimer.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class DeltaTimer : MonoBehaviour
 {
     public float TimerAmount = 10; // How long the countdown timer is
+    public float forceAmount = 50f; // How strong to make the frog's death force
     public string GoalMessage; // Instructions on how to play the game
     public string SuccessMessage; // WinGame message
     public string FailureMessage; // Game Over Message
@@ -13,12 +14,18 @@ public class DeltaTimer : MonoBehaviour
     public Text GameText; // The object with the "messages" text component
     public Text TimerText; // The object for the timer text
 
+    public GameObject frog; // The frog!
+    private Rigidbody frogrb; // Private variable for the frog's rigidbody
 
+    // TODO: Implement!!!!!!!! :>
+    private bool savedFrog; // Bool to check if player saved the frog
 
     // Start is called before the first frame update
     void Start()
     {
         GameText.text = GoalMessage; // Load in instructions
+        frogrb = frog.GetComponent<Rigidbody>(); // Init the frogrb variable
+        savedFrog = false;
     }
 
     // Update is called once per frame
@@ -32,12 +39,19 @@ public class DeltaTimer : MonoBehaviour
 
         TimerText.text = TimerAmount.ToString("f1"); // Constantly update timer text
 
-        if(TimerAmount<=0) // When it hits zero
+        if (TimerAmount <= 0) // When it hits zero
         {
             GameText.text = FailureMessage; // You have died
             TimerText.text = "0.0"; // Hard set the text if timer has expired
+            frogrb.AddForce(-frog.transform.forward * forceAmount, ForceMode.Impulse); // Force push the frog backward
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            print("here");
+
+        }
+
 
     }
 }
