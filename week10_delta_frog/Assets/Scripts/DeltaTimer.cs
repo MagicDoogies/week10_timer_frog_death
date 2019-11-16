4d6187a029a4545f21451efd4431d317ef2e0482
zeeodd <evergreenskier@gmail.com>
2019-11-16 09:13:08 -0500

    1. Formatted comments a bit 2. Added the countdown text and functionality 3. Increased instruction text size

--- week10_delta_frog/Assets/Scripts/DeltaTimer.cs
+++ week10_delta_frog/Assets/Scripts/DeltaTimer.cs
@@ -5,30 +5,37 @@ using UnityEngine.UI;
 
 public class DeltaTimer : MonoBehaviour
 {
-    public float TimerAmount = 10; //how long the countdown timer is
-    public string GoalMessage; //instructions on how to play the game
-    public string SuccessMessage; //WinGame message
-    public string FailureMessage; //Game Over Message
+    public float TimerAmount = 10; // How long the countdown timer is
+    public string GoalMessage; // Instructions on how to play the game
+    public string SuccessMessage; // WinGame message
+    public string FailureMessage; // Game Over Message
 
-    public Text GameText; //The object with the "messages" text component
-    public Text TimerText; //The object for the timer text
+    public Text GameText; // The object with the "messages" text component
+    public Text TimerText; // The object for the timer text
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        GameText.text = GoalMessage; //Load in instructions
+        GameText.text = GoalMessage; // Load in instructions
     }
 
     // Update is called once per frame
     void Update()
     {
-        TimerAmount -= Time.deltaTime; //countdown from timer amount
-
-        if(TimerAmount<=0) // when it hits zero
+        // Only decrement timer if above 0
+        if (TimerAmount >= 0)
         {
-            GameText.text = FailureMessage; //you have died
+            TimerAmount -= Time.deltaTime; // Countdown from timer amount
+        }
+
+        TimerText.text = TimerAmount.ToString("f1"); // Constantly update timer text
+
+        if(TimerAmount<=0) // When it hits zero
+        {
+            GameText.text = FailureMessage; // You have died
+            TimerText.text = "0.0"; // Hard set the text if timer has expired
         }
         
 
